bc6fe31c42504276af6b844a379899c4c2879bbc
juri.diener <dev.juri.diener@gmail.com>
2023-09-23 00:33:50 +0200

    add some modules to neorg

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/MyDocuments/notes/",
+            },
+            index = "index.norg",
+            default_workspace = "notes",
+          },
+        },
+        ["core.export"] = {},
+        ["core.summary"] = {},
+        ["core.integrations.telescope"] = {},
+        ["core.keybinds"] = {
+          config = {
+            neorg_leader = " ",
+          },
+        },
+      },
+    })
+  end,
+}
